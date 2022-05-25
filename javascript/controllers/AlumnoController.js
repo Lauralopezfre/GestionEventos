119f98cc94b06d495ee5d6f5dbd7a9ef56ba119b
Laura Edith López Fregoso <laura.lopez204820@potros.itson.edu.mx>
2022-05-24 23:57:37 -0500

    Arreglos API

--- javascript/controllers/AlumnoController.js
+++ javascript/controllers/AlumnoController.js
@@ -1,6 +1,7 @@
 const Alumno = require('../models/Alumno')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { default: mongoose } = require('mongoose')
 
 const register = (req, res, next) => {
     bcrypt.hash(req.body.contrasenna, 10, function(err, hashedPass){
@@ -10,40 +11,46 @@ const register = (req, res, next) => {
             })
         }
         let alumno = new Alumno({
-            _id: req.body.id,
+            _id: req.body._id,
             carrera: req.body.carrera,
             nombre: req.body.nombre,
             apellido: req.body.apellido,
-            contrasenna: hashedPass
+            contrasenna: hashedPass,
+            asistencia: req.body.asistencia
         })
-        alumno.save()
-        .then(alumno => {
-            res.json({
-                message: "Alumno guardado con éxito"
-            })
-        })
-        .catch(error=>{
-            res.json({
-                message: "Hubo un problema al guardar al alumno"
-            })
-        })
+        alumno.save(((err) => {
+            if (err) {
+              res.status(400).json({
+                status: "error",
+                message: `error al crear el evento: ${err}`,
+                error: err,
+              });
+            } else
+              res.status(201).json({
+                status: "success",
+                message: `evento creado con éxito`,
+              });
+          }));
     })
 }
 
 const login = (req, res, next) =>{
-    var id = req.body._id
+    console.log(req.body);
+
+    var id = req.body.id
     var contrasenna = req.body.contrasenna
-    Alumno.findOne({$or: [{_id:id}]})
-    .then(alumno => {
-        if(alumno){
-            bcrypt.compare(contrasenna, alumno.contrasenna, function(err, result){
+    Alumno.findOne({id:id})
+    .then(alu => {
+        console.log(alu)
+        if(alu){
+            bcrypt.compare(contrasenna, alu.contrasenna, function(err, result){
                 if(err){
                     res.json({
                         error: err
                     })
                 }
                 if(result){
-                    let token = jwt.sign({name: alumno.nombre}, 'verySecretValue', {expiresIn:'1h'})
+                    let token = jwt.sign({name: alu.nombre}, 'verySecretValue', {expiresIn:'1h'})
                     res.json({
                         message: 'Inicio de sesión correcto',
                         token
--- javascript/models/Alumno.js
+++ javascript/models/Alumno.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose')
 //La creación del documento dentro de la colleción en mongo
 var Schema = mongoose.Schema;
 var modeloAlumno = new Schema({
-    id: String,
+    _id: String,
     carrera: String,
     nombre: String,
     apellido: String,
     contrasenna: String,
     asistencia: Boolean
 });
-var modelo = mongoose.model('Alumno', modeloAlumno);
+var modelo = mongoose.model('alumno', modeloAlumno);
 module.exports = modelo;
